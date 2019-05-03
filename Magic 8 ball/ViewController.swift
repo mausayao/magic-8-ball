545dd8f4a2ed68a1be7b6589bf48b7512de8554c
mausayao <mauricio.sayao@gmail.com>
2019-05-03 16:56:51 -0400

    primeiro commit

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by Maurício de Freitas Sayão on 03/05/19.
+//  Copyright © 2019 Maurício de Freitas Sayão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
